1f6ceca7504da9af78ff3f20ae155f32324ee7ca
Andrew Conway <a.conway7@nuigalway.ie>
2017-04-13 16:11:09 +0100

    Program files
    Simple text output example.

---
+++ src/HelloVersion2.java
@@ -0,0 +1,10 @@
+
+public class HelloVersion2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Rosie, you are very small, yet delightful");
+
+	}
+
+}
